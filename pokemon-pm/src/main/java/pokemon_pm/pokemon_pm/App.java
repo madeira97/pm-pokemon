e28ed285d3c709e3eb2ffaadccd23880b454188b
madeira97 <ricardo.madeira11@hotmail.com>
2022-05-17 14:07:56 +0100

    maven

---
+++ pokemon-pm/src/main/java/pokemon_pm/pokemon_pm/App.java
@@ -0,0 +1,13 @@
+package pokemon_pm.pokemon_pm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
